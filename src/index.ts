adfd84c363b3819e20c004a4b6348f149f490049
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-06-18 15:51:45 +0200

    Add login routes, middlewares etc.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('It works!')
+import express, {Request, Response} from 'express';
+import {router} from "./router/loginRouter";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({keys: ['faagehq']}));
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
