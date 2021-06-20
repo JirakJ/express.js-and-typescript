dbefb47408e2d5bc41149001abc60e492fc4aa2b
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-06-20 16:59:14 +0200

    Integration express js and typescript example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import express, {Request, Response} from 'express';
-import {router} from "./router/loginRouter";
+import express from 'express';
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
+import './controllers/LoginController';
+import './controllers/RootController';
+import {AppRouter} from "./appRouter";
 
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({keys: ['faagehq']}));
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
